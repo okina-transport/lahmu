f08ccebae1b393f5265e7cf4359385d5a3ad6c58
Aryan Iranzamini <aryan.iranzamini@entur.org>
2020-04-28 08:54:46 +0200

    Add test folder for project (#3)

---
+++ test/ApplicationTest.kt
@@ -0,0 +1,11 @@
+package com.entur.test
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class ApplicationTest {
+    @Test
+    fun test() {
+        var t = 1
+        assertEquals(t, 1)
+    }
+}
