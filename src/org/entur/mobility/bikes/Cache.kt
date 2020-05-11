6ef0ac535f0aab68137345ece3eedbcfd7ceb1bf
Simen Johnsrud <simen.johnsrud92@gmail.com>
2020-05-11 13:40:25 +0200

    Add error handling

--- src/org/entur/mobility/bikes/Cache.kt
+++ src/org/entur/mobility/bikes/Cache.kt
@@ -1,16 +1,15 @@
 package org.entur.mobility.bikes
 
 import java.time.LocalDateTime
+import java.time.ZoneOffset
 import org.entur.mobility.bikes.bikeOperators.Operator
 
 interface Cache {
     val cacheMap: HashMap<Operator, HashMap<GbfsStandardEnum, GBFSResponse>>
-    val lastUpdated: LocalDateTime
 }
 
 class InMemoryCache(
-    override val cacheMap: HashMap<Operator, HashMap<GbfsStandardEnum, GBFSResponse>>,
-    override var lastUpdated: LocalDateTime
+    override val cacheMap: HashMap<Operator, HashMap<GbfsStandardEnum, GBFSResponse>>
 ) : Cache {
     fun getResponseFromCache(bikeOperator: Operator, gbfsStandardEnum: GbfsStandardEnum): GBFSResponse? {
         return cacheMap[bikeOperator]?.get(gbfsStandardEnum)
@@ -26,11 +25,14 @@ class InMemoryCache(
         } else {
             cacheMap[operator]!![gbfsStandardEnum] = response
         }
-        lastUpdated = LocalDateTime.now()
         return response
     }
 
     fun isValidCache(bikeOperator: Operator, gbfsStandardEnum: GbfsStandardEnum): Boolean =
-        cacheMap[bikeOperator]?.get(gbfsStandardEnum) != null && lastUpdated > LocalDateTime.now()
-            .minusSeconds(TIME_TO_LIVE_CACHE)
+        LocalDateTime.ofEpochSecond(
+            cacheMap[bikeOperator]?.get(gbfsStandardEnum)?.last_updated ?: 0L,
+            0,
+            ZoneOffset.UTC
+        ) > LocalDateTime.now()
+            .minusSeconds(TIME_TO_LIVE_CACHE_SEC)
 }
--- src/org/entur/mobility/bikes/constants.kt
+++ src/org/entur/mobility/bikes/constants.kt
@@ -1,4 +1,4 @@
 package org.entur.mobility.bikes
 
-const val POLL_INTERVAL = 60000L
-const val TIME_TO_LIVE_CACHE = 60000L
+const val POLL_INTERVAL_MS = 60000L
+const val TIME_TO_LIVE_CACHE_SEC = 60L
