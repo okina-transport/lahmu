157aa5f63ec9bf5e31458f55b2ed4fcc879938cc
Tom Erik Støwer <testower@gmail.com>
2021-08-12 14:56:00 +0200

    Remove transmodel style for system id

--- src/org/entur/lahmu/web/controllers/ProxyController.kt
+++ src/org/entur/lahmu/web/controllers/ProxyController.kt
@@ -133,10 +133,10 @@ class ProxyControllerImpl(private val bikeService: BikeService, private val cach
     }
 
     private fun mapSystemInformation(operator: Operator, result: GBFSResponse.SystemInformationResponse): SystemInformation {
-        var systemId = "${operator.getCodeSpace()}:System:${result.data.systemId}"
+        var systemId = result.data.systemId
 
         if (operator == Operator.TRONDHEIMBYSYKKEL) {
-            systemId = "${operator.getCodeSpace()}:System:${result.data.systemId}bysykkel"
+            systemId = "${result.data.systemId}bysykkel"
         }
 
         return SystemInformation(
